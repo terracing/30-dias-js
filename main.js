e35dcfd1c30520aa6116467b402970b86b5918e6
Omar Terrazas <terrazas.omar@gmail.com>
2023-03-04 05:10:08 +0000

    Building objects

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
- 
+ const natalia = {
+    name: "Natalia",
+    edad: 20,
+    cursosAprobados: [
+        "Curso Definitivo de HTML y CSS",
+        "Curso Práctico de HTML y CSS",
+    ],
+    aprobarCurso(nuevoCursito) {
+        this.cursosAprobados.push(nuevoCursito);
+    },
+ };
+
+ function Student(name, age, approvedCourses) {
+    this.name = name;
+    this.age = age;
+    this.approvedCourses = approvedCourses;
+ }
+
+ Student.prototype.aprobarCurso = function(nuevoCursito) {
+    this.approvedCourses.push(nuevoCursito);
+ }
+
+ const juanita = new Student(
+    "Juanita",
+    15,
+    ["Curso de introducción a los videojuegos","Curso de creación de videojuegos"]
+ )
